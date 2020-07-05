c4868dafdf3e57426bb7d8b9b2458700ae76e7c4
Omar Ryhan <omarryhan@gmail.com>
2020-07-05 16:33:30 +0200

    replaced ">0.25" with defaults in browser list

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+
+// postcss-preset-env and autoprefixer are already being imported by Next.js
 module.exports = {
   plugins: {
-    // autoprefixer. NextJS includes it
     'postcss-preset-env': {},
   },
 };
