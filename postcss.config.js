d37704b955847e27d2c39a30d30f4fcee988ab67
Omar Ryhan <omarryhan@gmail.com>
2020-07-10 13:03:39 +0200

    Removed redundant postcss dependancy

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable global-require */
-
-// postcss-preset-env and autoprefixer are already being imported by Next.js
-module.exports = {
-  plugins: {
-    'postcss-preset-env': {},
-  },
-};
