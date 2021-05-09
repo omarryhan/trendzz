e7334b1174e8c8b22415b813e9c98c672de44ddb
Omar Ryhan <omarryhan@gmail.com>
2021-05-09 16:13:51 +0200

    Remove unneeded local types file

--- pages/api/repositories.ts
+++ pages/api/repositories.ts
@@ -1,15 +1,8 @@
-// commenting this out because it's causing a type error
-// import { fetchRepositories } from '@huchenme/github-trending/lib/scrape';
 import { fetchRepositories } from '@huchenme/github-trending/lib/scrape';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   const language = req.query.language as string;
-  // Not working now
-  // I thought this function interfaced directly with the function
-  // that scrapes the data. Instead it calls github-trending's server
-  // that hosts the API, which is currently down. I'll use another API for
-  // now and leave this here should I need to replace it later.
   res.setHeader('Cache-Control', 's-maxage=86400');
   const response = await fetchRepositories({ language, since: 'daily' });
   res.json(response);
