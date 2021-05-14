c5e10c9d30add9aaed3fb6f34c9b9fd751cac4a4
Omar Ryhan <omarryhan@gmail.com>
2021-05-14 19:37:29 +0200

    Style tweaks

--- components/Header/Component.tsx
+++ components/Header/Component.tsx
@@ -6,42 +6,44 @@ import Settings from '../../public/settings.svg';
 
 const Component: React.FC<{ hideSettings?: boolean }> = ({ hideSettings = false }) => (
   <header className={styles.header}>
-    <div className={styles.logoAndTitleWrapper}>
-      <button
-        className={styles.logoWrapper}
-        onClick={(): Promise<boolean> => Router.push('/')}
-        type="button"
-      >
-        <Logo />
-      </button>
-      <h1 className={styles.title}>
-        Trendzz
-      </h1>
-    </div>
+    <div className={styles.headerContents}>
+      <div className={styles.logoAndTitleWrapper}>
+        <button
+          className={styles.logoWrapper}
+          onClick={(): Promise<boolean> => Router.push('/')}
+          type="button"
+        >
+          <Logo />
+        </button>
+        <h1 className={styles.title}>
+          Trendzz
+        </h1>
+      </div>
 
-    <div
-      className={styles.settingsWrapper}
-    >
-      {
-        !hideSettings
-          ? (
-            <button
-              className={styles.settingsButton}
-              type="button"
-              title="Settings"
-              onClick={(): void => {
-                Router.push('/settings');
-              }}
-            >
-              <div style={{ display: 'flex', height: '100%', alignItems: 'center' }}>
-                <Settings />
-              </div>
-            </button>
-          )
-          : (
-            null
-          )
-      }
+      <div
+        className={styles.settingsWrapper}
+      >
+        {
+          !hideSettings
+            ? (
+              <button
+                className={styles.settingsButton}
+                type="button"
+                title="Settings"
+                onClick={(): void => {
+                  Router.push('/settings');
+                }}
+              >
+                <div style={{ display: 'flex', height: '100%', alignItems: 'center' }}>
+                  <Settings />
+                </div>
+              </button>
+            )
+            : (
+              null
+            )
+        }
+      </div>
     </div>
   </header>
 );
