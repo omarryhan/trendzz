daa36c0bc2a8c7d16a973a83af37e0679cd6e75e
Omar Ryhan <omarryhan@gmail.com>
2021-06-21 01:43:24 +0200

    Caching for 3 hours instead of one day

--- pages/api/repositories.ts
+++ pages/api/repositories.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   const language = req.query.language as string;
   const since = req.query.since as string || 'daily';
-  res.setHeader('Cache-Control', 's-maxage=86400');
+  res.setHeader('Cache-Control', 's-maxage=10800');  // 3 hours
   const response = await fetchRepositories({ language, since });
   res.json(response);
 };
